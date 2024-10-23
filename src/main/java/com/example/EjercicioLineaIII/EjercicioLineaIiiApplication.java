8f64f7be714e750d82b847054303a5b53e7c609e
William Alexander Matallana <wmatallana28@gmail.com>
2024-10-22 19:25:00 -0500

    Ejercicio

---
+++ src/main/java/com/example/EjercicioLineaIII/EjercicioLineaIiiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioLineaIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioLineaIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioLineaIiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EjercicioLineaIII/EjercicioLineaIiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioLineaIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioLineaIiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
